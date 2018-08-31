d33fd2b3a1f80913c2120078229052d1de5c8f61
Johan Klinge <johan.klinge@gmail.com>
2018-08-31 13:56:30 +0200

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreTodo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TodoItem> Items { get; set; }
+        //fix for "Specified key was too long" error message in migrations. From https://www.junian.net/aspnet-core-mysql-ubuntu-16-04/
+        protected override void OnModelCreating(ModelBuilder builder)  
+        {  
+            base.OnModelCreating(builder);  
+        
+            builder.Entity<ApplicationUser>(entity => entity.Property(m => m.Id)  
+                .HasMaxLength(255));  
+            builder.Entity<ApplicationUser>(entity => entity.Property(m => m.NormalizedEmail)  
+                .HasMaxLength(255));  
+            builder.Entity<ApplicationUser>(entity => entity.Property(m => m.NormalizedUserName)  
+                .HasMaxLength(255));  
+        
+            builder.Entity<IdentityRole>(entity => entity.Property(m => m.Id)  
+                .HasMaxLength(255));  
+            builder.Entity<IdentityRole>(entity => entity.Property(m => m.NormalizedName)  
+                .HasMaxLength(255));  
+        
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.LoginProvider)  
+                .HasMaxLength(255));  
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.ProviderKey)  
+                .HasMaxLength(255));  
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.Property(m => m.UserId)  
+                .HasMaxLength(255));  
+
+            builder.Entity<IdentityUserRole<string>>(entity => entity.Property(m => m.UserId)  
+                .HasMaxLength(255));  
+            builder.Entity<IdentityUserRole<string>>(entity => entity.Property(m => m.RoleId)  
+                .HasMaxLength(255));  
+
+            builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.UserId)  
+                .HasMaxLength(255));  
+            builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.LoginProvider)  
+                .HasMaxLength(255));  
+            builder.Entity<IdentityUserToken<string>>(entity => entity.Property(m => m.Name)  
+                .HasMaxLength(255));  
+
+            builder.Entity<IdentityUserClaim<string>>(entity => entity.Property(m => m.Id)  
+                .HasMaxLength(255));  
+            builder.Entity<IdentityUserClaim<string>>(entity => entity.Property(m => m.UserId)  
+                .HasMaxLength(255));  
+            builder.Entity<IdentityRoleClaim<string>>(entity => entity.Property(m => m.Id)  
+                .HasMaxLength(255));  
+            builder.Entity<IdentityRoleClaim<string>>(entity => entity.Property(m => m.RoleId)  
+                .HasMaxLength(255));  
+        }
+
+    }
+}
---
+++ Data/Migrations/20180831093148_Initial.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetCoreTodo.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedUserName",
+                table: "AspNetUsers",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedEmail",
+                table: "AspNetUsers",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedName",
+                table: "AspNetRoles",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    IsDone = table.Column<bool>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    DueAt = table.Column<DateTimeOffset>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedUserName",
+                table: "AspNetUsers",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedEmail",
+                table: "AspNetUsers",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NormalizedName",
+                table: "AspNetRoles",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+    }
+}
