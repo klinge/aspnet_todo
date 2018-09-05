c417a46caaea7425191eec78fcd363c34ff944e7
Johan Klinge <johan.klinge@gmail.com>
2018-09-05 15:56:32 +0200

    Function to complete items added in view, controller, service

--- AspNetCoreTodo/wwwroot/js/site.js
+++ AspNetCoreTodo/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function() {
+
+    // Wire up all of the checkboxes to run markCompleted()
+    $('.done-checkbox').on('click', function(e) {
+        markCompleted(e.target);
+    });
+    
+    //add bootstrap datepicker on date input in AddItemPartial
+    $('.date-field').datepicker({
+        format: "yyyy-mm-dd",
+        startDate: "today",
+        weekStart: 1,
+        maxViewMode: 3,
+        todayBtn: "linked",
+        language: "sv",
+        autoclose: true,
+        todayHighlight: true
+    });
+
+});
+
+function markCompleted(checkbox) {
+    checkbox.disabled = true;
+
+    var row = checkbox.closest('tr');
+    $(row).addClass('done');
+
+    var form = checkbox.closest('form');
+    form.submit();
+}
